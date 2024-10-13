99fe3af06f52693eb294d608674244cf93d3cc30
pzk <beyondpzk@outlook.com>
2024-10-13 15:12:12 +0800

    update tmux

--- install_tmux.sh
+++ install_tmux.sh
@@ -6,4 +6,7 @@ cp .tmux/.tmux.conf.local .
 
 ## 但是要注意打开 ~/.tmux.conf.local 把tmux 设置成与自动开启 zsh
 
-可以将 tmux.conf 中的部分放到 .tmux.conf.local 中
+#可以将 tmux.conf 中的部分放到 .tmux.conf.local 中
+#set -g default-shell /bin/zsh
+#set -g default-command /bin/zsh
+#set -g default-terminal "xterm-256color"  # 与退出tmux执行 `echo $TERM` 保持一致.
