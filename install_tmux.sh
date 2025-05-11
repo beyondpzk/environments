cde927fe1d990b2c6d4e842bc5c0da1437d8e340
zpk <zpk>
2025-05-11 09:29:41 +0800

    update add

--- install_tmux.sh
+++ install_tmux.sh
@@ -1,12 +1,12 @@
-cd
+apt install -y tmux 
+HOME=$HOME
+cd $HOME/
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-
-
-## 但是要注意打开 ~/.tmux.conf.local 把tmux 设置成与自动开启 zsh
-
-#可以将 tmux.conf 中的部分放到 .tmux.conf.local 中
-#set -g default-shell /bin/zsh
-#set -g default-command /bin/zsh
-#set -g default-terminal "xterm-256color"  # 与退出tmux执行 `echo $TERM` 保持一致.
+cp .tmux/.tmux.conf.local $HOME/
+echo "set -g default-shell /bin/zsh"  >> "$HOME/.tmux.conf.local"
+echo "set -g default-command /bin/zsh"  >> "$HOME/.tmux.conf.local"
+echo "set -g default-terminal 'xterm-256color'"  >> "$HOME/.tmux.conf.local"  # 要和vim的保持一致.
+echo "set -g mouse on"  >> "$HOME/.tmux.conf.local"  # 要和vim的保持一致.
+echo "set clipboard=unnamedplus"  >> "$HOME/.tmux.conf.local"  # 要和vim的保持一致.
+tmux source-file $HOME/.tmux.conf.local
